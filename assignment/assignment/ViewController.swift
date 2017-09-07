b4e9e5db57fcc0b5a4ef296204f1ee81762a0065
Hien Nguyen <nlhien@fossil.com>
2017-09-07 08:49:08 +0700

    - init project and pod libs

---
+++ assignment/assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assignment
+//
+//  Created by Hien Nguyen on 9/7/17.
+//  Copyright © 2017 Misfit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
